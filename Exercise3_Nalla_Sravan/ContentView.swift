7ef1c71cf97c797c79efcff03c5eea0eeeed42e0
sravansai04 <sravansai766@gmail.com>
2023-09-08 20:16:20 -0500

    Initial Commit

---
+++ Exercise3_Nalla_Sravan/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Exercise3_Nalla_Sravan
+//
+//  Created by Sravan Sai Rahul Nalla on 9/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Exercise3_Nalla_Sravan/Exercise3_Nalla_SravanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise3_Nalla_SravanApp.swift
+//  Exercise3_Nalla_Sravan
+//
+//  Created by Sravan Sai Rahul Nalla on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Exercise3_Nalla_SravanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
